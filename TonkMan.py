67446acface9aa214ece136daad311093929a2c1
MarlonAnacona <ejemplo@email.com>
2022-05-24 19:42:43 -0500

    update

--- TonkMan.py
+++ TonkMan.py
@@ -1,2 +1,29 @@
 
+from sys import setswitchinterval
+
+
+menu ="""Elija la dificultad del juego(Escoja un numero): "+ 
+"\n 1. Facil " + 
+"\n 2. Medio" +
+"\n 3. Dificil"""
+opcionEscogida =int(input(menu))
+
+
+
+if opcionEscogida == 1:
+    MatrizJuego[7][7]
+    
+elif opcionEscogida ==2 :
+    pesos=input("Cuantos pesos chilenos tienes")
+    pesos=float(pesos)
+    valorDolar=2000
+    resultado= pesos/valorDolar
+elif opcionEscogida ==3:
+    pesos=input("Cuantos pesos chilenos tienes")
+    pesos=float(pesos)
+    valorDolar=2000
+    resultado= pesos/valorDolar
+else: 
+    print("Escoja una opcion correcta")
+
 
